6b56ae054ea620fb5f13ab4f4de2af387a7fe06e
chitranjanYti06 <142107399+chitranjanYti06@users.noreply.github.com>
2024-01-05 12:37:44 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+const accountId = 144553
+let accountEmail = "chitranjank862@gmail.com"
+var accountPassword = "12345"
+accountCity = "Varanasi"
+
+
+//accountId = 2 not allowed we cant change the value of const variable
+accountEmail = "vsjkkj@gmail.com"
+accountPassword = "25522"
+accountCity = "Delhi"
+
+let accountState;
+//if we are not assigning any value in the variable so it will be undefined
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+
+{
+
+    //scope
+    //
+    /*
+    prefer not to use var
+    because of issue in block scope and functional scope
+     */
+}
