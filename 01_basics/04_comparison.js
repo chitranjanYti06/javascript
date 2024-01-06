2575e90de0c85d4e459f6904ddc22f1c68e307da
chitranjanYti06 <142107399+chitranjanYti06@users.noreply.github.com>
2024-01-06 15:44:08 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log( 2 >= 1)
+// console.log(2 < 1)
+// console.log( 2 == 1)
+// console.log( 2 != 1)
+
+
+console.log("2" > 1)
+console.log("02" >1)
+
+
+console.log(null > 0)
+console.log( null == 0)
+console.log(null >= 0)
+
+//The reason in that an equality check == and comparison > <>= <= work differently
+// Comparison convert null to a number ,treating it as 0.
+
+console.log( undefined == 0)
+console.log( undefined > 0)
+console.log( undefined < 0)
+
+
+// === strict check
+
+console.log("2"===2);
