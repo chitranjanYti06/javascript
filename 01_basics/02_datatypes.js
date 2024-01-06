b5b2ea9920bbf73c841657f4c063ea36bc093838
chitranjanYti06 <142107399+chitranjanYti06@users.noreply.github.com>
2024-01-06 14:51:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";//treat all js code as the newer version
+
+// alert("hello")  we are using node js not browser
+
+//code readability should be high
+
+let name="chitranjan"//string
+let age=18 // number
+let isLoggedin = false //boolean
+
+let state = null
+
+
+//primitive datatypes
+
+//nmber => 2 to power 53
+//bigint => tradding stock market
+//strint we can use in both single and double quote
+//boolean => true/false
+//null => in actual it is standalone value
+//undefind => when we are not assigning any value
+
+//symbol to find the uniquness between two component used in figma 
+
+//object
+
+console.log(typeof "chitranjan")
+console.log(typeof null)// object
+console.log(typeof undefined)//undefined
