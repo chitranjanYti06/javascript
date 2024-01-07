dd10d6681c47954c906128ad68efd3c171bf1fad
chitranjanYti06 <142107399+chitranjanYti06@users.noreply.github.com>
2024-01-07 09:16:12 +0000

    changes in array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,4 +1,44 @@
 //array 
 
 const myArray = [0, 1 , 3 , 4 ,5]
-console.log(myarr[0]);
+
+
+const muHeroes = ["shaktiman","nagraj"]
+
+const myArray2 = new Array(1,2,3,7)
+
+console.log(myArray[0]);
+
+
+//array Method
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(0)
+
+// myArray.shift()
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(9));
+
+const newArr = myArray.join()
+
+console.log(myArray)
+console.log(typeof newArr)
+
+
+// slice ,splice
+
+console.log("A" , myArray);
+
+const myn1 = myArray.slice(1,3)
+
+console.log(myn1)
+console.log("B" , myArray)
+
+const myn2 = myArray.splice(1,3)
+console.log("c",myArray);
+
+console.log(myn2)
