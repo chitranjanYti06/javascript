02fa545c9501eb761aa794ee7cc8e20df0c86eb6
chitranjanYti06 <142107399+chitranjanYti06@users.noreply.github.com>
2024-01-08 10:01:27 +0000

    while and do while

---
+++ 05_loops/two.js
@@ -0,0 +1,25 @@
+// while
+
+
+// let index = 0
+// while (index <= 10) {
+//     // console.log(` Value of index is ${index}`);
+//     index = index + 2
+    
+// }
+
+let myArray = [" flash ", "batman", "Superman"]
+
+let arr = 0
+while(arr < myArray.length){
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+//do while loop
+
+let score = 11
+do {
+    console.log(` Score is ${score}`);
+    score++
+} while (score <= 10);
