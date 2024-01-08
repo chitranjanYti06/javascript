2c7356897a560f0a98bcb658298c38bbcc0e744d
chitranjanYti06 <142107399+chitranjanYti06@users.noreply.github.com>
2024-01-08 09:20:53 +0000

    changes

--- 04_controlFlow/truthyANDFalsy.js
+++ 04_controlFlow/truthyANDFalsy.js
@@ -24,3 +24,24 @@ if(Object.keys(emptyObj).length === 0){
     console.log("object is empty")
 }
 
+//Nullish Coaleacing Operator(??) :null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 null ?? 10
+
+// val1 = undefined ?? 15
+
+val1 = null ?? 10 ?? 15
+
+console.log(val1)
+
+
+// ternary operator
+
+// condition ? true : false
+
+
+const iceTeaPrice = 100
+
+iceTeaPrice <=80 ? console.log("less than 80"):console.log("more than 80")
