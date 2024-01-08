66774c2a78cc96a4dadfeae0d9d2ab93e3eb4268
chitranjanYti06 <142107399+chitranjanYti06@users.noreply.github.com>
2024-01-08 08:58:52 +0000

    swich

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,27 @@
+// switch(key){
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch(month){
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("feburary")
+        break;  
+    case 3:
+        console.log("march")
+        // break;
+    case 4:
+         console.log("april")
+        break;  
+
+    default:
+        console.log("default case matched")    
+}
