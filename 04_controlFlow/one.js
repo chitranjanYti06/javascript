3cc3ce7991dccf1799a3e69be4287c4b2115fa89
chitranjanYti06 <142107399+chitranjanYti06@users.noreply.github.com>
2024-01-08 08:49:05 +0000

    conditional done

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,61 @@
+//if
+
+const isUserLogge5dIn = true
+const temperature = 41
+
+if (temperature < 50){
+    console.log("less than 50")
+
+}
+else{
+    console.log("temerature is greater than 50")
+}
+console.log("execute")
+
+
+const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(` user power : ${power}`)
+// }
+// console.log(`User power : ${power}`);
+
+const balance = 1000
+
+// if(balance > 500)console.log("teat2"),console("test");
+
+// if(balance < 500){
+//     console.log("less than")
+// } else if(balance < 750){
+//     console.log("less than 750")
+// } else if(balance < 900){
+//     console.log("less than 900")
+// }
+// else{
+//     console.log("less than 1200")
+// }
+
+
+
+
+
+
+
+
+// // < , >, <= ,>= , == ,!== , === , !==
+
+
+const isUserLoggedIn = true 
+const debitcard = true
+
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(isUserLoggedIn && debitcard && 2==3){
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
+}
