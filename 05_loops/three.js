0a07667b5d09ec3d4dbb87b3b8269629fdf8fda4
chitranjanYti06 <142107399+chitranjanYti06@users.noreply.github.com>
2024-01-08 10:15:43 +0000

    for of loop

---
+++ 05_loops/three.js
@@ -0,0 +1,30 @@
+// for of
+
+// ["" ,"",""]
+// [{}, {}, {}]
+
+const arr =[ 1,2,3,4]
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "Hello World!"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
+}
+
+
+// Maps 
+
+const map = new Map()
+map.set('In' , "India")
+map.set('USA' ,"United States of America")
+map.set('Fr', "France")
+map.set('In' , "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, '-' ,value);
+}
